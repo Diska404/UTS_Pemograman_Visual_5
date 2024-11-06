19fe52d49acd13d8de08a30f7ef332cd7793574d
diska <diskakurniaputra@gmail.com>
2024-11-06 13:57:52 +0700

    Update

---
+++ Hello Word/src/hello/word/HelloWord.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.word;
+
+/**
+ *
+ * @author ACER
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public class Main {
+    public static void main(String[] args) {
+        // Menampilkan pesan Hello World
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Helloword/src/helloword/Latihan1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helloword;
+
+/**
+ *
+ * @author ACER
+ */
+public class Latihan1 {
+    
+    public static void main(String[] args) {
+        System.out.println("7+2");
+        System.out.println(7+2);
+    }
+}
